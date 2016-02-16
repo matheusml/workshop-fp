826b122c2150275fb23d37352d750b51a16ca8e3
Matheus Lima <matheusml90@gmail.com>
2016-02-16 20:30:32 -0200

    Adding map + filter

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ describe('Functional Programming Workshop', function() {
   
   describe('Pure Functions', function () {
     it('returns the days in the month', function () {
+      // impure
       var daysThisMonth = function() {
         var date  = new Date();
         var year  = date.getFullYear();
@@ -13,6 +14,7 @@ describe('Functional Programming Workshop', function() {
         return Math.round((end - start) / (1000 * 60 * 60 * 24));
       };
       
+      // impure
       var daysInMonth = function(year, month) {
         var start = new Date(year, month - 1, 1);
         var end   = new Date(year, month, 1);
@@ -56,7 +58,9 @@ describe('Functional Programming Workshop', function() {
   describe('Map', function () {
     it('returns the square roots of the numbers', function() {
       var numbers = [1, 4, 9];
-      var roots = numbers.map(Math.sqrt);
+      var roots = numbers.map(function(number) {
+          return Math.sqrt(number);
+      });
       
       assert.deepEqual(roots, [1, 2, 3]);
     });
@@ -68,11 +72,48 @@ describe('Functional Programming Workshop', function() {
           {name: 'Maria', grade: 9}
       ];
       
-      var grades = function(students) {
-        
-      };
+      var grades = function() {};
       
       assert.deepEqual(grades(students), [6, 4, 9]);
+    });
+  });
+  
+  describe('Filter', function () {
+    it('returns the numbers bigger than 4', function() {
+      var numbers = [1, 4, 9];
+      var filteredNumbers = numbers.filter(function(number) {
+          return number > 4;
+      });
+      
+      assert.deepEqual(filteredNumbers, [9]);
+    });
+    
+    it('returns the students with the grade bigger than or equal to 6', function () {
+      var students = [
+          {name: 'Anna',  grade: 6},
+          {name: 'John',  grade: 4},
+          {name: 'Maria', grade: 9}
+      ];
+      
+      var filterApprovedStudents = function() {};
+      
+      assert.deepEqual(filterApprovedStudents(), 
+        [{name: 'Anna',  grade: 6},
+         {name: 'Maria', grade: 9}]);
+    });
+  });
+  
+  describe('Map + Filter', function () {
+    it('returns the names of the students with the grade bigger than or equal to 6', function () {
+      var students = [
+          {name: 'Anna',  grade: 6},
+          {name: 'John',  grade: 4},
+          {name: 'Maria', grade: 9}
+      ];
+      
+      var filterApprovedStudentsByName = function() {};
+      
+      assert.deepEqual(filterApprovedStudentsByName(), ['Anna',  'Maria']);
     });
   });
   
